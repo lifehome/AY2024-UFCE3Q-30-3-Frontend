c3a77e7feee31f6616fb8dd1711bdd89a780e91d
Ivan Ip <m@lifeho.me>
2025-05-06 00:38:31 +0800

    ⚙️ patch: add back branch route for page navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
   },
+  { 
+    path: 'branches',
+    loadComponent: () => import('./branches/branches.component').then(m => m.BranchesComponent)
+  },
 ];
