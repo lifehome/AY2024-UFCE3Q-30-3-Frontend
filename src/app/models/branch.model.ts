1b6e7f9d77ba21f712fbf6f726878c6ae9861b41
Ivan Ip <m@lifeho.me>
2025-05-06 21:00:38 +0800

    🫧 chore: add remark to barrier-free_access_code

--- src/app/models/branch.model.ts
+++ src/app/models/branch.model.ts
@@ -7,5 +7,10 @@ export interface Branch {
   latitude: string;
   longitude: string;
   'barrier-free_access': string;
+
+  /**
+   * This is a new property discovered as of 2025-05-06,
+   * but nothing is returned in the API response yet.
+   */
   'barrier-free_access_code': string | null;
 }
